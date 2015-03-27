60857319c86d3f05521c45745d83b914ef81aee4
Kevin McGrath <kevin.mcgrath@USLT-205529.sungardas.corp>
2015-03-26 19:11:48 -0500

    remove bower and standard partials

--- gulpfile.js
+++ gulpfile.js
@@ -4,8 +4,10 @@ var config = {
   s3: [
     {
       aws: {
-        region: 'us-east-1'
+        region: 'us-east-1',
+        bucket: ''
       },
+      labels: ['dev'],
       validate: false,
       create: false
     }
