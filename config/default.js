ea5a99aa431cdb257f69a04b1135a9e63149829f
Kevin McGrath <kevin.mcgrath@sungardas.com>
2015-09-24 05:57:59 +0000

    Multi-Region RDS replication templates

--- config/default.js
+++ config/default.js
@@ -1,11 +1,85 @@
+var gitrev = require('git-rev-sync');
+
 var config = {
   s3: [
     {
       aws: {
         region: 'us-east-1',
-        bucket: ''
+        bucket: 'condensation-examples.us-east-1'
       },
-      labels: ['dev'],
+      prefix: '/'+gitrev.branch(),
+      validate: true,
+      create: false
+    },
+    {
+      aws: {
+        region: 'us-west-2',
+        bucket: 'condensation-examples.us-west-2'
+      },
+      prefix: '/'+gitrev.branch(),
+      validate: true,
+      create: false
+    },
+    {
+      aws: {
+        region: 'us-west-1',
+        bucket: 'condensation-examples.us-west-1'
+      },
+      prefix: '/'+gitrev.branch(),
+      validate: true,
+      create: false
+    },
+    {
+      aws: {
+        region: 'eu-west-1',
+        bucket: 'condensation-examples.eu-west-1'
+      },
+      prefix: '/'+gitrev.branch(),
+      validate: true,
+      create: false
+    },
+    {
+      aws: {
+        region: 'eu-central-1',
+        bucket: 'condensation-examples.eu-central-1'
+      },
+      prefix: '/'+gitrev.branch(),
+      validate: true,
+      create: false
+    },
+    {
+      aws: {
+        region: 'ap-southeast-1',
+        bucket: 'condensation-examples.ap-southeast-1'
+      },
+      prefix: '/'+gitrev.branch(),
+      validate: true,
+      create: false
+    },
+    {
+      aws: {
+        region: 'ap-southeast-2',
+        bucket: 'condensation-examples.ap-southeast-2'
+      },
+      prefix: '/'+gitrev.branch(),
+      validate: true,
+      create: false
+    },
+    {
+      aws: {
+        region: 'ap-northeast-1',
+        bucket: 'condensation-examples.ap-northeast-1'
+      },
+      prefix: '/'+gitrev.branch(),
+      validate: true,
+      create: false
+    },
+    {
+      aws: {
+        region: 'sa-east-1',
+        bucket: 'condensation-examples.sa-east-1'
+      },
+      prefix: '/'+gitrev.branch(),
       validate: true,
       create: false
     }
